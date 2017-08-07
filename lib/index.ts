2ea695b3974095f6931859761e7179c113708ebb
Bruno Jouhier <bjouhier@gmail.com>
2017-08-07 21:23:45 +0200

    fix typing issue

--- lib/index.ts
+++ lib/index.ts
@@ -134,7 +134,7 @@ export function reader<T>(connection: tds.Connection, sql: string, args: any[], 
 	opts.fillParameters(request, args);
 
 	// set up listeners
-	request.on('row', function (row) {
+	request.on('row', function (row: T) {
 		if (stopped) return;
 		if (tracer) tracer('ROW', row);
 		send(null, row);
